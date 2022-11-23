2478deeac2e40f7d95776aa526528c027b14a21c
Gowthamboppa <gowthamboppa@gail.com>
2022-11-23 18:09:45 +0530

    1st change

---
+++ variable.py
@@ -0,0 +1,5 @@
+x=10 # x is a variaxle loaction where the value 10 is assigining
+# variable name can be any letter or word etc
+print(x) # print function is used to print the value of x in output
+# changing
+# again changing
